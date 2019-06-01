f44e3d317a7c63dab4ce8a4ce7fb8ebbb561ad3f
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-06-01 20:14:24 -0300

    Agora toda vez que o usuário faz logout, ele é redirecionado para a tela principal do app

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
   canActivate(): Promise<boolean> {
     return new Promise(resolve => {
       this.authService.getAuth().onAuthStateChanged(user => {
-        if (!user) this.router.navigate(['login']);
+        if (!user) this.router.navigate(['home']);
 
         resolve(user ? true : false);
       });
