50ef542d2ad422e38c7ec9a75b0650ee398d444f
projetoAssoar <projetoassoar@gmail.com>
2019-06-02 23:38:21 -0300

    Botões apenas para teste das telas de doador e paciente

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DoadorPageModule } from '../doador/doador.module';
+import { PacientPageModule } from '../pacient/pacient.module';
 
 
 @Component({
@@ -9,16 +11,15 @@ import { Router } from '@angular/router';
 })
 export class AboutPage implements OnInit {
   constructor(public router: Router) { }
-
   ngOnInit() {
   }
 
-  souPaciente() {
-    this.router.navigate(['pacient']);
+  btn_Doador() {
+    this.router.navigate(['doador']);
   }
 
-  souDoador() {
-    this.router.navigate(['doador']);
+  btn_Paciente() {
+    this.router.navigate(['pacient']);
   }
 
 }
